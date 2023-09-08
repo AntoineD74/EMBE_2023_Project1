0e00b83f228635a86ee23e8e4f72677cfc89c2b9
antoine <antoine.dupuis74@icloud.com>
2023-09-08 14:50:32 +0000

    Part 3 done

--- src/main.cpp
+++ src/main.cpp
@@ -43,38 +43,37 @@
 //   return 0;
 // }
 
-bool c1_hi = false;
-bool c2_hi = false;
-bool clockwise = false;
 
 //Part 3
+bool c1_hi = false;
+bool clockwise = false;
+
+Digital_out led(5); //Built-in LED
+Encoder encoder(2, 3); //c1: D2, c2: D3
+
 int main()
 {
-  Digital_out led(5); //Built-in LED
-  Encoder encoder(4, 5); //c1: D2, c2: D3
+  Serial.begin(9600);
  
   led.init();
   encoder.init();
 
-  DDRD &= ~(1 << DDD1); // set the PD1 pin as input
-  PORTD |= (1 << PORTD1); // enable pull-up resistor on PD1
-  EICRA |= (1 << ISC00); // set INT0 on rising edge
-  EIMSK |= (1 << INT0); // Turns on INT0
-
   DDRD &= ~(1 << DDD2); // set the PD2 pin as input
   PORTD |= (1 << PORTD2); // enable pull-up resistor on PD2
-  EICRA |= (1 << ISC01); // set INT1 on rising edge
-  EIMSK |= (1 << INT1); // Turns on INT1
-
+  EICRA |= (1 << ISC01) | (1 << ISC00); // set INT0 on rising edge
+  EIMSK |= (1 << INT0); // Turns on INT0
   sei();
 
   while(1)
   {
-    if(c1_hi && c2_hi)
+    _delay_us(200);
+    
+    if(c1_hi)
     {
       encoder.updateCounter(clockwise);
+      Serial.println(encoder.getPosition());
       c1_hi = false;
-      c2_hi = false;
+      clockwise = false;
 
       led.toggle();
     }
@@ -86,13 +85,8 @@ int main()
 ISR (INT0_vect)
 {
   c1_hi = true;
-  if(c2_hi==false)
+  if(!encoder.is_C2_hi())
   {
     clockwise = true;
   }
-}
-
-ISR (INT1_vect)
-{
-  c2_hi = true;
 }
